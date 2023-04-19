c1e79695b05e536d6becb9d264190799672c5608
AsemDeve <120917153+AsemDeve@users.noreply.github.com>
2023-04-19 12:09:16 +0200

    formatting

--- read_questions.py
+++ read_questions.py
@@ -18,28 +18,25 @@ choices_pattern_2 = "^[(][a-fA-F][.|\-|)|:]"
 choices_pattern_by_letter=choices_pattern_1 + '|' + choices_pattern_2
 
 header = ['questionType', 'questionText', 'option1', 'option2', 'option3', 'option4', 'option5', 'answer']
+check_image_list = ['image', 'shown', 'figure']
 
 matches = re.split(question_pattern, doc, flags=re.DOTALL)  # to split answers and questions :
 
 list_of_questions = matches[1::2]  # Elements from "matches" list starting from 1 iterating by 2
 list_of_choices = matches[2::2]  # Elements from "matches" list starting from 2 iterating by 2
-check_image_list = ['image', 'shown', 'figure']
 
 list_of_questions_with_images=[]
 list_of_questions_with_duplicate_choices=[]
 question_and_choices = []
 question_and_choices.append(header)
-
 duplicated_questions=0
 
 for question, choices in zip(list_of_questions, list_of_choices):
     if any(word.lower() in question.lower() for word in check_image_list): # chcek if questions have images
         list_of_questions_with_images.append(str(list_of_questions.index(question) + 1))
-
     if '$' in question:  # to ignore duplicated questions, while dublicate questions have ($) in it
         duplicated_questions+=1
     else:
-
         inner_list = ["mc"]
         removeH = re.sub(r"#", "", question)  # remove # from question Text
         cleanQ = re.sub(r"^(\d{1,3}[.|\-|)|:])", "", removeH.strip())  # remove question number
@@ -49,22 +46,17 @@ for question, choices in zip(list_of_questions, list_of_choices):
         for choice in raw_split_choices:  # remove empty choices from choices list
             if choice.strip() != '':
                 split_choices.append(choice.strip())
-
         answer_index = 0
         for item in split_choices:
             cleanChoice = re.sub(choices_pattern_by_letter, "", item.strip())  # remove choices numbering
-
             if '*' in item:
                 answer_index = split_choices.index(item)+1
                 cleanChoice = re.sub("\*", "", item)  # remove * from the answer.
                 cleanChoice = re.sub(choices_pattern_by_letter, "",cleanChoice.strip())  # remove choices numbering from the answer
-
             if not cleanChoice.endswith(".") and cleanChoice.strip()!="" : # add (.) to the end of choices.
-
                     inner_list.append(cleanChoice.strip() + '.')
             else:
                     inner_list.append(cleanChoice.strip())
-
         if len(inner_list)!=len(set(inner_list)): # check duplicates  in choices
             list_of_questions_with_duplicate_choices.append(list_of_questions.index(question)+1)
         inner_list.append(None)  # to keep list and answer index wrapped.
